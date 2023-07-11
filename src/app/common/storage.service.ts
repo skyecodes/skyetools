cc3ab0201a33d3c7a9b013dc187584dc116b350d
skyecodes <skyecodes@proton.me>
2023-07-11 11:22:39 +0200

    feat: improved file download

--- src/app/common/storage.service.ts
+++ src/app/common/storage.service.ts
@@ -1,28 +1,14 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
-import {tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
 
-  constructor(private http: HttpClient) {
-  }
-
   download(fileId: string) {
-    return this.http.get(environment.apiUrl + '/storage/' + fileId, {
-      responseType: 'blob',
-      observe: 'response'
-    }).pipe(tap(response => {
-      let downloadUrl = window.URL.createObjectURL(response.body!);
-      let filename = response.headers.get('Content-Disposition')!.substring(22);
-      filename = filename.substring(0, filename.length - 1);
-      let link = document.createElement('a');
-      link.href = downloadUrl;
-      link.download = filename;
-      link.click();
-    }));
+    let link = document.createElement('a');
+    link.href = environment.apiUrl + '/storage/' + fileId;
+    link.click();
   }
 }
